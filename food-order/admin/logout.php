705d97f4520020800fbea533fc1a9a4b58545c62
nikhilreddysoreddy <118904318+nikhilreddysoreddy@users.noreply.github.com>
2025-04-10 18:19:54 +0530

    Delete food-order/admin directory

--- food-order/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //include constants.php for SITEURL
-    include('../config/constants.php');
-    //1.destroy session 
-    session_destroy();//unsets $_session['user']
-    //2.redirect to login page
-    header('location:'.SITEURL.'admin/login.php');
-?>
